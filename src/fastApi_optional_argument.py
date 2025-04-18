9a036b3a10764b58e9f6133137559634d9389889
Moji <94471127+MojiJokar@users.noreply.github.com>
2025-04-18 09:44:51 +0200

    Update fastApi_optional_argument.py

--- src/fastApi_optional_argument.py
+++ src/fastApi_optional_argument.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from typing import Optional
+api = FastAPI()
 
+
+
+@api.get('/addition')
+def get_addition(a: int, b: Optional[int]=None):
+    if b:
+        result = a + b
+    else:
+        result = a + 1
+    return {
+        'addition_result': result
+    }
+
+
+class Item(BaseModel):
+    itemid: int
+    description: str
+    owner: Optional[str] = None
